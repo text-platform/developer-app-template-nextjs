3e6229ae8dfe943a399b6b108c6d8d886d58bd63
wojciechdudek-livechat <117641044+wojciechdudek-livechat@users.noreply.github.com>
2024-01-25 09:00:44 +0100

    Upgraded developer-sdk

--- hooks/app/useDeveloperApp.ts
+++ hooks/app/useDeveloperApp.ts
@@ -2,13 +2,17 @@ import { useEffect, useState } from 'react'
 import { DeveloperApp, DeveloperAppConfig } from '@livechat/developer-sdk'
 import lcConfig from '../../livechat.config.json'
 
+const config = lcConfig as DeveloperAppConfig;
+
 function useDeveloperApp() {
   const [developerApp, setDeveloperApp] = useState<DeveloperApp | null>(null)
 
   useEffect(() => {
-    DeveloperApp.init(lcConfig as DeveloperAppConfig)
+    DeveloperApp.init(config as DeveloperAppConfig)
       .then(async (app) => {
-        await app.authorize()
+        if (config.auth?.clientId) {
+          await app.authorize()
+        }
 
         return app
       })
